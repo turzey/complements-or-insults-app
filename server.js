77fde87b1c07aeffc849df7b739e8aac89213154
Turzey <benidibatia@gmail.com>
2020-01-23 22:29:38 +0200

    added package.json file and have modified some code lines in the server.js file

--- server.js
+++ server.js
@@ -13,24 +13,6 @@ const complements = [
   "the world needs more of that"
 ];
 
-const insults = [
-  "why don't you listen ?",
-  "i told you not to you dick!",
-  "get the fuck out of here bitch!",
-  "oh dear , i can see you really got no ear ...",
-  "i warned you !",
-  "it's your fault.",
-  "that was a good lesson",
-  "next time you won't try to do this again",
-  "fuck you!!🤣🤣🤣"
- ];
-
-
-
-
-
-
-
 function getRandomComplement() {
   const randomIndex = Math.floor(Math.random() * complements.length);
   return complements[randomIndex];
@@ -53,36 +35,11 @@ app.get("/complement", function(req, res) {
 
 app.use("/public", express.static("./public"));
 
-app.listen(3000);
-console.log("listening on http://localhost:3000");
+
+const port = process.env.PORT || 3000;
+app.listen(port);
+console.log(`listening on http://localhost:${port}`);
 
 
 
-
- function getRandominsult() {
- const randomIndex = Math.floor(Math.random() * insults.length);
- return insults[randomIndex];
- }
-
-
-
- app.get("/", function(req, res) {
- res.sendFile(path.join(__dirname, "index.html"));
-});
-
-app.get("/insults", function(req, res) {
- res
-     .json({
-      insults: getRandominsult()
-     })
-       .end();
-   });
-
-   app.use("/public", express.static("./public"));
-
-   app.listen(3000);
-   console.log("listening on http://localhost:3000");
-
-
-
-
+ 
